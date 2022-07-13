134e7d50a79458afadc8751b3aadf4f9e682340c
avorada <93083556+avorada@users.noreply.github.com>
2022-07-13 15:08:52 +0300

    Add files via upload
    примеры кода из первой главы под названием числа

---
+++ Chapter1.py
@@ -0,0 +1,9 @@
+print ('hello world') # do notes with help of #
+print (345+7)
+print(len(str(2**100000)))# сколлько цифр в числе 2 в 100000 степени
+3*4
+import math
+print(math.pi)#число пи
+import random
+print(random.random())#рандоиное число в интеравале от 0 до 1
+print(random.choice([1.3,2.67,45]))#рандоиное число из списка
