55c39fc10f99a8133038145e16f3a2991fb4bc9e
Ada Sheverova <ada.sheverova@gmail.com>
2024-12-01 17:15:04 +0300

    тест

---
+++ 1.py
@@ -0,0 +1,13 @@
+my_name = "Avor Ad"
+my_age = 19
+my_height = 173.3
+my_weight = 63.5
+my_eyes = "green and brown"
+my_hair = "red"
+my_universaty = "BSU"
+print(f"I'm {my_name}, who is writting this program.")
+print(f"My height {my_height} cm.")
+print(f"My weight {my_weight} kg.")
+print(f"The colors of my eyes is {my_eyes} and the colors of my hair is {my_hair}.")
+total=my_age+my_height+my_weight
+print(f"The sum of {my_age}, {my_height}, {my_weight} is {total}." )
